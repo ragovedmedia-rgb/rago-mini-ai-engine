699d9180228280830c580a170f577b984c00fad6
ragovedmedia-rgb <ragovedmedia@gmail.com>
2026-02-27 16:43:11 +0530

    Update main.py

--- categories/color/auto_lut_generator/main.py
+++ categories/color/auto_lut_generator/main.py
@@ -1,19 +1,9 @@
-from .processor import generate_lut
-from engine.response import success, error
+from categories.color.auto_lut_generator.processor import generate_lut
 
 def run(data):
-    try:
-        lut_path = generate_lut(
-            data.get("source"),
-            data.get("reference"),
-            float(data.get("intensity", 1)),
-            int(data.get("lut_size", 33))
-        )
-
-        return success({
-            "success": True,
-            "lut_path": lut_path
-        })
-
-    except Exception as e:
-        return error(str(e))
+    return generate_lut(
+        data.get("source"),
+        data.get("reference"),
+        data.get("intensity", 1),
+        data.get("lut_size", 33)
+    )
