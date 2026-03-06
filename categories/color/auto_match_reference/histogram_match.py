d2229ddcd129d6ba8d47543ea3b3e195e401dbdc
ragovedmedia-rgb <ragovedmedia@gmail.com>
2026-03-06 22:48:54 +0530

    Create histogram_match.py

---
+++ categories/color/auto_match_reference/histogram_match.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+def histogram_match(src_img, ref_img):
+
+    src_yuv = cv2.cvtColor(src_img, cv2.COLOR_BGR2YUV)
+    ref_yuv = cv2.cvtColor(ref_img, cv2.COLOR_BGR2YUV)
+
+    src_y = src_yuv[:,:,0]
+    ref_y = ref_yuv[:,:,0]
+
+    src_hist,_ = np.histogram(src_y.flatten(),256,[0,256])
+    ref_hist,_ = np.histogram(ref_y.flatten(),256,[0,256])
+
+    src_cdf = src_hist.cumsum()
+    ref_cdf = ref_hist.cumsum()
+
+    src_cdf = src_cdf / src_cdf[-1]
+    ref_cdf = ref_cdf / ref_cdf[-1]
+
+    lookup = np.zeros(256)
+
+    for i in range(256):
+        diff = np.abs(ref_cdf - src_cdf[i])
+        lookup[i] = np.argmin(diff)
+
+    matched = cv2.LUT(src_y, lookup.astype("uint8"))
+
+    src_yuv[:,:,0] = matched
+
+    return cv2.cvtColor(src_yuv, cv2.COLOR_YUV2BGR)
