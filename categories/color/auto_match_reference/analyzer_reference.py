5c77b81a1b702caa0b9cdd8a265c1ad91c9ffcd2
ragovedmedia-rgb <ragovedmedia@gmail.com>
2026-03-06 22:31:52 +0530

    Create analyzer_reference.py

---
+++ categories/color/auto_match_reference/analyzer_reference.py
@@ -0,0 +1,21 @@
+import numpy as np
+from .utils import compute_luma
+
+def analyze_reference(img):
+
+    luma = compute_luma(img)
+
+    black = np.percentile(luma,5)
+    white = np.percentile(luma,95)
+    mid = np.median(luma)
+
+    contrast = white - black
+    brightness = np.mean(luma)
+
+    return {
+        "black": float(black),
+        "white": float(white),
+        "mid": float(mid),
+        "contrast": float(contrast),
+        "brightness": float(brightness)
+    }
