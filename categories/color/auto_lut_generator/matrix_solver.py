92e975f181e7d7d212286e4ada883ed504a6ddf7
ragovedmedia-rgb <ragovedmedia@gmail.com>
2026-03-06 16:48:30 +0530

    Update matrix_solver.py

--- categories/color/auto_lut_generator/matrix_solver.py
+++ categories/color/auto_lut_generator/matrix_solver.py
@@ -1,8 +1,23 @@
+import numpy as np
+
+# Example color transform matrix
+# This simulates reference color shift
+
+COLOR_MATRIX = np.array([
+    [1.05, 0.02, -0.02],
+    [0.01, 1.03, -0.01],
+    [-0.02, 0.02, 1.05]
+])
+
+
 def apply_matrix(r, g, b):
 
-    # Identity matrix for now
-    rr = r
-    gg = g
-    bb = b
+    rgb = np.array([r, g, b])
+
+    transformed = COLOR_MATRIX.dot(rgb)
+
+    rr = max(0.0, min(1.0, transformed[0]))
+    gg = max(0.0, min(1.0, transformed[1]))
+    bb = max(0.0, min(1.0, transformed[2]))
 
     return rr, gg, bb
