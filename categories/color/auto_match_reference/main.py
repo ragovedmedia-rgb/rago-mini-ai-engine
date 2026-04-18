a023d41558169582083e1c6b53a63ff62ca83fcf
ragovedmedia-rgb <ragovedmedia@gmail.com>
2026-04-18 11:07:09 +0530

    Update main.py

--- categories/color/auto_match_reference/main.py
+++ categories/color/auto_match_reference/main.py
@@ -65,11 +65,13 @@ def run(data):
         # (Log/Gamma → Linear normalization)
         # ----------------------------------------
 
-        ref_img = prepare_for_analysis(ref_img)
-        src_img = prepare_for_analysis(src_img)
-
+       # STEP 1: match in normal space (0–255)
         matched = histogram_match(src_img, ref_img)
         src_img = zone_harmony(matched, ref_img)
+
+        # STEP 2: convert for analysis (linear)
+        ref_img = prepare_for_analysis(ref_img)
+        src_img = prepare_for_analysis(src_img)
         
 
         # ----------------------------------------
